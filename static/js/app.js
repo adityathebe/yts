e52dd76117991162a6ecb37c04b10beda43c2a7d
adityathebe <thebeaditya@gmail.com>
2017-11-06 19:35:50 +0545

    minimum Rating 7

--- static/js/app.js
+++ static/js/app.js
@@ -12,7 +12,7 @@ let app = new Vue({
     methods: {
         getMovies: function(page = 1, q = '') {
             console.log('Getting movies...')
-            let url = YTS_API + `list_movies.jsonp?page=${page}&query_term=${q}&limit=50`;
+            let url = YTS_API + `list_movies.jsonp?page=${page}&query_term=${q}&limit=50&minimum_rating=7`;
 
             this.memory.queryTerm = q.length > 0 ? q : '';
             this.movies = [];
